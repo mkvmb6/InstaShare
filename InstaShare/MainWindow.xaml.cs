153112b5e5064a2388f8b16f4e091b04857c3fce
Mohit Kumar Verma <mkvmb.6@gmail.com>
2025-07-06 16:16:55 +0530

    Created basic UI with file select enabled.

--- InstaShare/MainWindow.xaml.cs
+++ InstaShare/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,34 @@ namespace InstaShare
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Select a File to Upload";
+            if (dlg.ShowDialog() == true)
+            {
+                string selectedFilePath = dlg.FileName;
+                SelectedPathText.Text = "Selected File: " + selectedFilePath;
+                // TODO: Pass this path to your upload logic
+            }
+        }
+
+        private void SelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            //using (var dlg = new System.Windows.Forms.FolderBrowserDialog())
+            //{
+            //    dlg.Description = "Select a Folder to Upload";
+            //    dlg.UseDescriptionForTitle = true;
+
+            //    if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //    {
+            //        string selectedFolderPath = dlg.SelectedPath;
+            //        SelectedPathText.Text = "Selected Folder: " + selectedFolderPath;
+            //        // TODO: Pass this path to your folder upload logic
+            //    }
+            //}
         }
     }
 }
