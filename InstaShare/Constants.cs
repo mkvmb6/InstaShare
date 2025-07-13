fb056e26c2b8dc53a37a92621fde6aebbb3df158
Mohit Kumar Verma <mkvmb.6@gmail.com>
2025-07-14 00:16:22 +0530

    Implement file and folder upload correclty with cloudflare R2.

--- InstaShare/Constants.cs
+++ InstaShare/Constants.cs
@@ -5,5 +5,7 @@
         public const string AppName = "InstaShare";
         public const string R2BucketName = "instashare";
         public const string UploadedFilesJson = "uploadedFiles.json";
+        public const string AppBaseUrl = "https://instashare.mohitkumarverma.com/view";
+        public const string CdnBaseUrl = "https://cdn.instashare.mohitkumarverma.com";
     }
 }
--- InstaShare/FileManagers/IFileManager.cs
+++ InstaShare/FileManagers/IFileManager.cs
@@ -8,7 +8,7 @@
         /// <param name="filePath">The path of the file to upload.</param>
         /// <param name="reportProgress">An optional action to report upload progress.</param>
         /// <returns>A task that represents the asynchronous operation, containing the URL of the uploaded file.</returns>
-        Task<(string fileId, string sharedLink)> UploadFile(string filePath, string parentFolderId, Action<double, string, string> reportProgress = null);
+        Task<(string fileId, string sharedLink)> UploadFile(string filePath, string parentFolderId, Action<double, string, string>? reportProgress = null);
 
         /// <summary>
         /// Retrieves the path of an existing folder or creates a new folder if it does not exist.
@@ -44,7 +44,7 @@
         /// string describing the current operation.</param>
         /// <returns>A task that represents the asynchronous operation. The task result contains the ID of the created root
         /// folder on the remote drive.</returns>
-        Task<(string folderId, string sharedLink)> UploadFolderWithStructure(string localRootPath, string driveRootFolderName, Action<string, string> statusCallback = null);
+        Task<(string folderId, string sharedLink)> UploadFolderWithStructure(string localRootPath, string driveRootFolderName, Action<string, string>? statusCallback = null);
 
         /// <summary>
         /// Deletes a file from the service.
